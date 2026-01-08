897be3c310a458579e9352b21f4d0c8e726236a0
BeastMarck <tacaljlvlg@gmail.com>
2026-01-08 13:53:02 +0800

    Add files via upload

---
+++ OOP Array/OOP Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song[] songs = new Song[5];
+
+            songs[0] = new GameSong("IVORY Tower", "Hiroyuki SAWANO · SennaRin", "July 12, 2024", "Dragon Raja");
+            songs[1] = new GameSong("Bury the Light", "Casey Edwards", "August 26, 2024", "Devil May Cry");
+            songs[2] = new GameSong("Wildfire", "HOYO-Mix", "May 1, 2023", "Honkai: Star Rail");
+            songs[3] = new BandSong("Bismarck", "Sabaton", " Dec 31, 2019", "English");
+            songs[4] = new BandSong("Color", "FLOW", "Nov 5, 2013", "Japanese");
+
+            foreach (Song song in songs)
+            {
+                song.Diplay();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OOP Array/OOP Array/Song.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Array
+{
+    class Song
+    {
+        //Variables
+        public string Title { get; set; }
+        public string Composer { get; set; }
+        public string ReleaseDate { get; set; }
+        //Constructor
+        public Song(string title, string composer, string releasedate)
+        {
+            Title = title;
+            Composer = composer;
+            ReleaseDate = releasedate;
+        }
+        //Method
+        public virtual void Diplay()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Composer: {Composer}");
+            Console.WriteLine($"Release Date {ReleaseDate}");
+            Console.WriteLine("");
+        }
+    }
+
+    class GameSong : Song //child class
+    {
+        //Variable
+        public string Game { get; set; }
+        //Constructor
+        public GameSong(string title, string composer, string releasedate, string game) : base(title, composer, releasedate)
+        {
+            Game = game;
+        }
+        //Method
+        public override void Diplay()
+        {
+            Console.WriteLine("-----Song-----");
+            Console.WriteLine($"Game: {Game}");
+            base.Diplay();
+        }
+    }
+
+    class BandSong : Song //child class
+    {
+        //Variable
+        public string Language { get; set; }
+        //Constructor
+        public BandSong(string title, string composer, string releasedate, string language) : base(title, composer, releasedate)
+        {
+            Language = language;
+        }
+        //Method
+        public override void Diplay()
+        {
+            Console.WriteLine("-----Song-----");
+            Console.WriteLine($"Language: {Language}");
+            base.Diplay();
+        }
+    }
+}
